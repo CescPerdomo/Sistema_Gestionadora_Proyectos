acb532ba831fb1bc672aab06193a1f760235e1dc
Cesar Perdomo <cesarp1080@gmail.com>
2025-03-23 16:11:05 -0600

    Se agrega importacion dinamica de bootstrap del lado del cliente

--- app/clientWrapper.js
+++ app/clientWrapper.js
@@ -1,13 +1,19 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/queryClient";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { useEffect } from "react";
+
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
 import { AuthProvider } from "@/context/AuthContext";
 
 export default function ClientWrapper({ children }) {
+  useEffect(() => {
+    // Importa Bootstrap JS dinámicamente en el lado del cliente
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
