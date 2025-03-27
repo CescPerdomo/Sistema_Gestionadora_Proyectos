b1d68dfaa9317f0973c6607553af6836580ccc8b
Fabricio Castro <fabriciocastro002@gmail.com>
2025-03-27 05:45:46 -0600

    Layout.js issue, Error de Hidratación en tu Layout fixed

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-//layout sin "use client"; //aca indicamos que es un componente client, porque se usara internamente
-
-// app/layout.js (Server Component)
+// app/layout.js
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import ClientWrapper from "./clientWrapper";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <head>
         <title>Sistema de Gestión de Proyectos</title>
+        {/* Asegúrate de que los estilos se carguen correctamente */}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
       </head>
-      <body>
+      <body className="bg-light">
         <ClientWrapper>
           {children}
         </ClientWrapper>
